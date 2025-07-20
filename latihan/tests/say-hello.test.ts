15660d1a8e5d9225aa1c76d8a7b399841bd2537f
unknown <delkanoberutu8@gmail.com>
2025-07-20 07:09:30 +0700

    unit test

---
+++ latihan/tests/say-hello.test.ts
@@ -0,0 +1,7 @@
+import { sayHello } from "../src/say-hello";
+
+describe('sayHello', function () {
+    it('should be return hello miwa', function() {
+        expect(sayHello('miwa')).toBe('Hello miwa')
+    });
+});
